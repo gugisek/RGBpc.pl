e5daecc6ddc7a1a84c45c3c4b495aa1cd6e3fb44
gugisek <gugisek@gmail.com>
2023-06-15 10:33:10 +0200

    Add WSZM: user edit logic

--- WSZM-RGBpc.pl/components/users.php
+++ WSZM-RGBpc.pl/components/users.php
@@ -1,6 +1,5 @@
 <section class="w-full flex flex-col items-center px-5 pt-5 gap-4">
-    <section class="w-full bg-white shadow-xl rounded-3xl py-6 px-6 flex">
-        
+    <section class="w-full bg-white shadow-xl rounded-3xl py-6 px-6 flex">      
         <form action="panel.php?page=użytkownicy&action=" method="POST" class="w-full flex gap-6">
             <button type="submit" id="subm">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500 mr-2">
@@ -81,6 +80,139 @@
             });
         </script>
     </section>
+    <?php
+$action = $_GET['action'];
+if ($action=="edit") {
+    $id = $_GET['id'];
+    $sql = "SELECT users.id, users.name, users.mail, users.sur_name, user_roles.role, users.create_date, user_status.status FROM `users` join user_roles on users.role_id=user_roles.id join user_status on user_status.id=users.status_id where users.id=$id;";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0)
+    {
+        while($row = mysqli_fetch_assoc($result))
+        {
+            $create_date = $row['create_date'];
+            echo "<section id='edit' class='w-full bg-white shadow-xl rounded-3xl py-6 px-6'>";
+            echo "<h1 class='pb-2 font-medium text-gray-600 font-[Lexend]'>Edytuj użytkownika</h1>";
+            echo "<form action='scripts/edit_user.php' method='post' class='flex flex-col gap-4'>";
+            echo "<input type='hidden' name='id' value='".$row['id']."'>";
+            echo "<div class='flex flex-col gap-2'>";
+            echo "<label for='name' class='text-xs text-gray-500'>Imię</label>";
+            echo "<input type='text' name='name' id='name' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none text-gray-700 focus:text-gray-800 transition-all duration-300 focus:ring-2 focus:ring-green-500 focus:border-transparent' value='".$row['name']."'>";
+            echo "</div>";
+            echo "<div class='flex flex-col gap-2'>";
+            echo "<label for='sur_name' class='text-xs text-gray-500'>Nazwisko</label>";
+            echo "<input type='text' name='sur_name' id='sur_name' class='w-full py-2 px-4 rounded-lg text-gray-700 focus:text-gray-800 transition-all duration-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent' value='".$row['sur_name']."'>";
+            echo "</div>";
+            echo "<div class='flex flex-col gap-2'>";
+            echo "<label for='mail' class='text-xs text-gray-500'>E-mail</label>";
+            echo "<input type='text' name='mail' id='mail' class='w-full py-2 px-4 rounded-lg shadow-sm text-gray-700 focus:text-gray-800 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent' value='".$row['mail']."'>";
+            echo "</div>";
+            echo "<div class='flex flex-col gap-2'>";
+            echo "<label for='role' class='text-xs text-gray-500'>Rola</label>";
+            echo "<select name='role' id='role' class='w-full py-2 px-4 rounded-lg shadow-sm text-gray-700 focus:text-gray-800 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'>";
+            $sql2 = "SELECT * FROM `user_roles`;";
+            $result2 = mysqli_query($conn, $sql2);
+            if(mysqli_num_rows($result2) > 0)
+            {
+                while($row2 = mysqli_fetch_assoc($result2))
+                {
+                    echo "<option value='".$row2['id']."'";
+                    if ($row2['role']==$row['role']) {
+                        echo " selected";
+                    }
+                    echo ">".$row2['role']."</option>";
+                }
+            }
+            echo "</select>";
+            echo "</div>";
+            echo "<div class='flex flex-col gap-2'>";
+            echo "<label for='status' class='text-xs text-gray-500'>Status</label>";
+            echo "<select name='status' id='status' class='w-full py-2 px-4 rounded-lg shadow-sm text-gray-700 focus:text-gray-800 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'>";
+            $sql3 = "SELECT * FROM `user_status`;";
+            $result3 = mysqli_query($conn, $sql3);
+            if(mysqli_num_rows($result3) > 0)
+            {
+                while($row3 = mysqli_fetch_assoc($result3))
+                {
+                    echo "<option value='".$row3['id']."'";
+                    if ($row3['status']==$row['status']) {
+                        echo " selected";
+                    }
+                    echo ">".$row3['status']."</option>";
+                }
+            }
+            echo "</select>";
+            echo "</div>";
+            echo "<div class='flex flex-col gap-2'>";
+            echo "<label for='create_date' class='text-xs text-gray-500'>Data utworzenia</label>";
+            echo "<input type='text' name='create_date' id='create_date' class='w-full py-2 px-4 rounded-lg shadow-sm text-gray-700 focus:text-gray-800 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent' value='".substr($create_date, 0, strrpos($create_date, ' ', 0))."' disabled>";
+            echo "</div>";
+            echo "<div class='flex flex-col gap-2'>";
+            echo "<label for='password' class='text-xs text-gray-500'>Hasło</label>";
+            echo "<input type='password' name='password' id='password' class='w-full py-2 px-4 rounded-lg shadow-sm text-gray-700 focus:text-gray-800 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'>";
+            echo "</div>";
+            echo "<div class='flex flex-col gap-2'>";
+            echo "<label for='password2' class='text-xs text-gray-500'>Powtórz hasło</label>";
+            echo "<input type='password' name='password2' id='password2' class='w-full py-2 px-4 rounded-lg shadow-sm text-gray-700 focus:text-gray-800 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'>";
+            echo "</div>";
+            echo "<div class='flex flex-row gap-2'>";
+            echo "<button type='submit' class='w-full py-2 px-4 bg-green-500 hover:bg-green-600 hover:shadow-green-500 shadow-xl rounded-lg text-white font-medium transition-all duration-300'>Zapisz</button>";
+            echo "<a href='?page=użytkownicy&action=' class='w-full py-2 px-4 bg-red-500 text-center hover:bg-red-600 hover:shadow-red-500 shadow-xl rounded-lg text-white font-medium transition-all duration-300'>Anuluj</a>";
+            echo "</div>";
+            echo "</form>";
+            echo "</section>";
+        }
+    }
+    else
+    {
+        echo "<section class='w-full bg-white shadow-xl rounded-3xl py-6 px-6 mt-6'>";
+        echo "<h1 class='pb-2 font-medium text-gray-600 font-[Lexend]'>Edytuj użytkownika</h1>";
+        echo "<p class='text-sm text-gray-500'>Nie znaleziono użytkownika o podanym ID.</p>";
+        echo "</section>";
+    }
+    // notification for output of edit user
+}elseif($action=='edited') {
+    echo "<section id='edited' class='flex justify-between w-full bg-green-500 text-white shadow-green-300 shadow-xl rounded-3xl py-6 px-6 mt-6'>";
+    echo "<h1>Pomyślnie edytowano użytkownika.</h1>";
+    echo '<a href="?page=użytkownicy&action=" class="hover:rotate-90 active:scale-75 transition-all duration-300"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg></a>
+  ';
+    echo "</section>";
+}elseif($action=='error_pswd') {
+    echo "<section id='edited' class='flex justify-between w-full bg-red-500 text-white shadow-red-300 shadow-xl rounded-3xl py-6 px-6 mt-6'>";
+    echo "<h1>Podane hasła są różne!</h1>";
+    echo '<a href="?page=użytkownicy&action=" class="hover:rotate-90 transition-all duration-300"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg></a>
+  ';
+    echo "</section>";
+}elseif($action=='error') {
+    echo "<section id='edited' class='flex justify-between w-full bg-red-500 text-white shadow-red-300 shadow-xl rounded-3xl py-6 px-6 mt-6'>";
+    echo "<h1>Błąd bazy danych. Spróbuj ponownie lub skontaktuj się z administratorem.</h1>";
+    echo '<a href="?page=użytkownicy&action=" class="hover:rotate-90 transition-all duration-300"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg></a>
+  ';
+    echo "</section>";
+}elseif($action=='edited_pswd') {
+    echo "<section id='edited' class='flex justify-between w-full bg-green-500 text-white shadow-green-300 shadow-xl rounded-3xl py-6 px-6 mt-6'>";
+    echo "<h1>Pomyślnie edytowano użytkownika oraz jego hasło.</h1>";
+    echo '<a href="?page=użytkownicy&action=" class="hover:rotate-90 active:scale-75 transition-all duration-300"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg></a>
+  ';
+    echo "</section>";
+}elseif($action=='edited_same') {
+    echo "<section id='edited' class='flex justify-between w-full bg-orange-500 text-white shadow-orange-300 shadow-xl rounded-3xl py-6 px-6 mt-6'>";
+    echo "<h1>Podane hasło nie różni się od poprzedniego.</h1>";
+    echo '<a href="?page=użytkownicy&action=" class="hover:rotate-90 active:scale-75 transition-all duration-300"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+  </svg></a>
+  ';
+    echo "</section>";
+}
+?>
     <section class="w-full bg-white shadow-xl rounded-3xl py-6 px-6">
         <h1 class="pb-2 font-medium text-gray-600 font-[Lexend]">Użytkownicy</h1>
         <table class="w-full">
@@ -130,7 +262,7 @@
                     echo $row['status'];
                     echo "</td>";
                     echo "<td class='text-center text-sm text-gray-500'>".substr($create_date, 0, strrpos($create_date, ' ', 0))."</td>";
-                    echo "<td class='text-center text-sm'><a href='?page=użytkownicy&action=edit&id=".$row['id']."' class='text-indigo-500'>Edytuj</a></td>";
+                    echo "<td class='text-center text-sm'><a href='?page=użytkownicy&action=edit&id=".$row['id']."#edit' class='text-indigo-500 hover:text-[1rem] transition-all duration-300 '>Edytuj</a></td>";
                     echo "</tr>";
                     ;
                 }
@@ -148,96 +280,4 @@
             ?>
         </table>
     </section>
-<?php
-$action = $_GET['action'];
-if ($action=="edit") {
-    $id = $_GET['id'];
-    $sql = "SELECT users.id, users.name, users.mail, users.sur_name, user_roles.role, users.create_date, user_status.status FROM `users` join user_roles on users.role_id=user_roles.id join user_status on user_status.id=users.status_id where users.id=$id;";
-    $result = mysqli_query($conn, $sql);
-    if(mysqli_num_rows($result) > 0)
-    {
-        while($row = mysqli_fetch_assoc($result))
-        {
-            $create_date = $row['create_date'];
-            echo "<section class='w-full bg-white shadow-xl rounded-3xl py-6 px-6 mt-6'>";
-            echo "<h1 class='pb-2 font-medium text-gray-600 font-[Lexend]'>Edytuj użytkownika</h1>";
-            echo "<form action='scripts/edit_user.php' method='post' class='flex flex-col gap-4'>";
-            echo "<input type='hidden' name='id' value='".$row['id']."'>";
-            echo "<div class='flex flex-col gap-2'>";
-            echo "<label for='name' class='text-sm text-gray-500'>Imię</label>";
-            echo "<input type='text' name='name' id='name' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent' value='".$row['name']."'>";
-            echo "</div>";
-            echo "<div class='flex flex-col gap-2'>";
-            echo "<label for='sur_name' class='text-sm text-gray-500'>Nazwisko</label>";
-            echo "<input type='text' name='sur_name' id='sur_name' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent' value='".$row['sur_name']."'>";
-            echo "</div>";
-            echo "<div class='flex flex-col gap-2'>";
-            echo "<label for='mail' class='text-sm text-gray-500'>E-mail</label>";
-            echo "<input type='text' name='mail' id='mail' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent' value='".$row['mail']."'>";
-            echo "</div>";
-            echo "<div class='flex flex-col gap-2'>";
-            echo "<label for='role' class='text-sm text-gray-500'>Rola</label>";
-            echo "<select name='role' id='role' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'>";
-            $sql2 = "SELECT * FROM `user_roles`;";
-            $result2 = mysqli_query($conn, $sql2);
-            if(mysqli_num_rows($result2) > 0)
-            {
-                while($row2 = mysqli_fetch_assoc($result2))
-                {
-                    echo "<option value='".$row2['id']."'";
-                    if ($row2['role']==$row['role']) {
-                        echo " selected";
-                    }
-                    echo ">".$row2['role']."</option>";
-                }
-            }
-            echo "</select>";
-            echo "</div>";
-            echo "<div class='flex flex-col gap-2'>";
-            echo "<label for='status' class='text-sm text-gray-500'>Status</label>";
-            echo "<select name='status' id='status' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'>";
-            $sql3 = "SELECT * FROM `user_status`;";
-            $result3 = mysqli_query($conn, $sql3);
-            if(mysqli_num_rows($result3) > 0)
-            {
-                while($row3 = mysqli_fetch_assoc($result3))
-                {
-                    echo "<option value='".$row3['id']."'";
-                    if ($row3['status']==$row['status']) {
-                        echo " selected";
-                    }
-                    echo ">".$row3['status']."</option>";
-                }
-            }
-            echo "</select>";
-            echo "</div>";
-            echo "<div class='flex flex-col gap-2'>";
-            echo "<label for='create_date' class='text-sm text-gray-500'>Data utworzenia</label>";
-            echo "<input type='text' name='create_date' id='create_date' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent' value='".substr($create_date, 0, strrpos($create_date, ' ', 0))."' disabled>";
-            echo "</div>";
-            echo "<div class='flex flex-col gap-2'>";
-            echo "<label for='password' class='text-sm text-gray-500'>Hasło</label>";
-            echo "<input type='password' name='password' id='password' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'>";
-            echo "</div>";
-            echo "<div class='flex flex-col gap-2'>";
-            echo "<label for='password2' class='text-sm text-gray-500'>Powtórz hasło</label>";
-            echo "<input type='password' name='password2' id='password2' class='w-full py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent'>";
-            echo "</div>";
-            echo "<div class='flex flex-row gap-2'>";
-            echo "<button type='submit' class='w-full py-2 px-4 bg-green-500 hover:bg-green-600 rounded-lg text-white font-medium'>Zapisz</button>";
-            echo "<a href='?page=użytkownicy&action=' class='w-full py-2 px-4 bg-red-500 hover:bg-red-600 rounded-lg text-white font-medium'>Anuluj</a>";
-            echo "</div>";
-            echo "</form>";
-            echo "</section>";
-        }
-    }
-    else
-    {
-        echo "<section class='w-full bg-white shadow-xl rounded-3xl py-6 px-6 mt-6'>";
-        echo "<h1 class='pb-2 font-medium text-gray-600 font-[Lexend]'>Edytuj użytkownika</h1>";
-        echo "<p class='text-sm text-gray-500'>Nie znaleziono użytkownika o podanym ID.</p>";
-        echo "</section>";
-    }
-}
-?>
 </section>
---
+++ WSZM-RGBpc.pl/scripts/edit_user.php
@@ -0,0 +1,39 @@
+<?php
+include 'conn_db.php';
+$id = $_POST['id'];
+$name = $_POST['name'];
+$surname = $_POST['sur_name'];
+$mail = $_POST['mail'];
+$role_id = $_POST['role'];
+$status_id = $_POST['status'];
+$password = $_POST['password'];
+$password2 = $_POST['password2'];
+if ($password == $password2 && !empty($password) && !empty($password2)) {
+    $password = hash('sha256', $password);
+    $sql = "SELECT pswd FROM users WHERE id='$id'";
+    $result = mysqli_query($conn, $sql);
+    $row = $result->fetch_assoc();
+    if ($row['pswd'] == $password) {
+        header('Location: ../panel.php?page=użytkownicy&action=edited_same#edited');
+    } else {
+        $sql = "UPDATE users SET name='$name', sur_name='$surname', mail='$mail', role_id='$role_id', status_id='$status_id', pswd='$password' WHERE id='$id'";
+        if ($conn->query($sql) === TRUE) {
+            header('Location: ../panel.php?page=użytkownicy&action=edited_pswd#edited');
+        } else {
+            header('Location: ../panel.php?page=użytkownicy&action=error');
+        }
+    }
+
+} elseif (empty($password) && empty($password2)) {
+    
+        $sql = "UPDATE users SET name='$name', sur_name='$surname', mail='$mail', role_id='$role_id', status_id='$status_id' WHERE id='$id'";
+        if ($conn->query($sql) === TRUE) {
+            header('Location: ../panel.php?page=użytkownicy&action=edited#edited');
+        } else {
+            header('Location: ../panel.php?page=użytkownicy&action=error');
+        }
+    
+}else {
+    header('Location: ../panel.php?page=użytkownicy&action=error_pswd');
+}
+?>
