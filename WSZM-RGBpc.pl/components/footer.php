2ea093f0053b23ea2d7dcc00d17a34832201f5b4
gugisek <gugisek@gmail.com>
2023-07-31 17:40:26 +0200

    Add version badge to footer

--- WSZM-RGBpc.pl/components/footer.php
+++ WSZM-RGBpc.pl/components/footer.php
@@ -1,3 +1,5 @@
 <footer class="font-[Lexend] text-center font-light text-xs py-5 px-2">
 © 2021 - <?php echo date("Y"); ?>, created by <span class="text-red-600">R</span><span class="text-green-600">G</span><span class="text-blue-600">B</span>pc.pl for <span class="text-red-600">R</span><span class="text-green-600">G</span><span class="text-blue-600">B</span>pc.pl <br class="sm:hidden">powered by <a href="https://ui.gugisek.pl" class="hover:text-indigo-700 transition-all duration-100">gugisek</a> & lef
+    <br>
+    <span class="text-gray-500">version beta 0.1.0</span>
 </footer>    
